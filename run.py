b892c89890f81340dcf167fd589cb8d3800f475e
ajayg51 <ajayg.ug18.cs@nitp.ac.in>
2024-04-10 07:22:44 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from flask import Flask
-# import subprocess
+from app import create_app
+
 import git
 
-app = Flask(__name__)
+app = create_app()
 
 REPO_URL = "https://github.com/ajayg51/action-repo"
 REPO_LOCAL_PATH = "/Volumes/ajaySafeBox/0_projects/dev/0_self/3_python/action-repo"
@@ -23,34 +24,34 @@ def print_git_changes(diffChanges):
             print(chg.b_path)
 
 
-@app.route("/", methods=["GET"])
-def get_listen_for_changes_on_action_repo():
+# @app.route("/", methods=["GET"])
+# def get_listen_for_changes_on_action_repo():
     
-    print("repo")
-    print(repo)
-    print("local - head")
-    print(LOCAL_REPO_LAST_HEAD_COMMIT_ID_HASH)
+#     print("repo")
+#     print(repo)
+#     print("local - head")
+#     print(LOCAL_REPO_LAST_HEAD_COMMIT_ID_HASH)
 
-    remoteRepo = repo.remotes.origin
+#     remoteRepo = repo.remotes.origin
 
-    remoteRepo.fetch()
+#     remoteRepo.fetch()
 
-    REPO_REMOTE_HEAD_COMMIT_ID_HASH = remoteRepo.refs["main"].object.hexsha
+#     REPO_REMOTE_HEAD_COMMIT_ID_HASH = remoteRepo.refs["main"].object.hexsha
     
-    print("remote - head")
-    print(REPO_REMOTE_HEAD_COMMIT_ID_HASH)
+#     print("remote - head")
+#     print(REPO_REMOTE_HEAD_COMMIT_ID_HASH)
     
-    if LOCAL_REPO_LAST_HEAD_COMMIT_ID_HASH != REPO_REMOTE_HEAD_COMMIT_ID_HASH:
-        diff = LOCAL_REPO_LAST_HEAD_COMMIT_ID.diff(REPO_REMOTE_HEAD_COMMIT_ID_HASH)
-        print("diff")
-        print(diff)
-        print_git_changes(diff.change_type)
+#     if LOCAL_REPO_LAST_HEAD_COMMIT_ID_HASH != REPO_REMOTE_HEAD_COMMIT_ID_HASH:
+#         diff = LOCAL_REPO_LAST_HEAD_COMMIT_ID.diff(REPO_REMOTE_HEAD_COMMIT_ID_HASH)
+#         print("diff")
+#         print(diff)
+#         print_git_changes(diff.change_type)
 
-        msg = "Remote action-repo changed"
-        print(msg)
-        return msg
+#         msg = "Remote action-repo changed"
+#         print(msg)
+#         return msg
     
-    return "No git action detected"
+#     return "No git action detected"
 
 if __name__ == "__main__":
     app.run(host="localhost",port=4000,debug=False)
